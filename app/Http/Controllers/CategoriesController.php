8549fe4519cc33d2e7b1f37c6b52e4797964af30
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-08-15 13:10:44 -1000

    able to delete category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -96,8 +96,10 @@ class CategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        session()->flash('success', 'Category Delete Successfully');
+        return redirect(route('categories.index'));
     }
 }
