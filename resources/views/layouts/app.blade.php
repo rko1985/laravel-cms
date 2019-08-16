15d26dcd0590c062ad3806c54c0d34ff345091ca
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-08-16 07:23:25 -1000

    create post form finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,7 @@
         
                             <ul class="list-group">
                                 <li class="list-group-item">
-                                    <a href="">Posts</a>
+                                <a href="{{ route('posts.index')}}">Posts</a>
                                 </li>
                                 <li class="list-group-item">
                                 <a href="{{ route('categories.index')}}">Categories</a>
