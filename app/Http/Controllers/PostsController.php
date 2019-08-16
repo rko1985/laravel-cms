edf7818e42841e03b1a446cf0053b7de52171a8c
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-08-16 12:27:25 -1000

    able to soft/hard delete posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -101,6 +101,24 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::withTrashed()->where('id', $id)->firstOrFail();
+
+        if($post->trashed()){
+            $post->forceDelete();
+        } else {
+            $post->delete();
+        }
+
+        session()->flash('success', 'Post deleted successfully.');
+        return redirect(route('posts.index'));
+
+    }
+
+    //displays a list of all trashed posts
+    public function trashed(){
+
+        $trashed = Post::withTrashed()->get();
+        return view('posts.index')->with('posts', $trashed);
+
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,31 +10,47 @@
 
 <div class="card card-default">
 
-    <div class="card-header">
-
-        
-    </div>
+    <div class="card-header">Posts</div>
 
     <div class="card-body">
-        <table class="table">
-            <thead>
-                <th>Image</th>
-                <th>Title</th>
-                <th></th>
-                <th></th>
-            </thead>
-            <tbody>
-                    @foreach($posts as $post) 
-                        <tr>
-                            <td><img src="{{asset('/storage/'.$post->image)}}" width="60px"></td>
-                            <td>{{$post->title}}</td>
-                            <td><a href="" class="btn btn-info btn-sm">Edit</a></td>
-                            <td><a href="" class="btn btn-danger btn-sm">Trash</a></td>
+        @if($posts->count() > 0)
+            <table class="table">
+                <thead>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                        @foreach($posts as $post) 
+                            <tr>
+                                <td><img src="{{asset('/storage/'.$post->image)}}" width="60px"></td>
+                                <td>{{$post->title}}</td>
+                                <td>
+                                    @if(!$post->trashed())
+                                    <a href="" class="btn btn-info btn-sm">Edit</a>
+                                    @endif
+                                </td>
+                                <td>
+                                    <form action="{{route('posts.destroy', $post->id)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">
+                                            {{ $post->trashed() ? 'Delete' : 'Trash'}}
+                                        </button>
+                                    </form>
+                                </td>
 
-                        </tr>
-                    @endforeach
-            </tbody>
-        </table>
+                            </tr>
+                        @endforeach
+                </tbody>
+            </table>
+
+        @else
+
+        <h3 class="text-center">No Posts Yet</h3>
+
+        @endif
     </div>
 
 </div>
